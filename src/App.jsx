195fc8aca6f5cdc6ecbf3938ec865cc5e1b0ea24
piyush254 <76161817+piyush254@users.noreply.github.com>
2024-10-02 20:47:01 +0530

    pdf downloader

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import MyPage from './Components/MyPage'
 import { PDFViewer } from '@react-pdf/renderer'
 import DownloadPdf from './Components/DownloadPdf'
-
+import './Components/Pdf.css'
 function App() {
   return (
     <>
     {/* <PDFViewer style={{width : "100vw" , height : "97vh"}}>
       <MyPage />
     </PDFViewer> */}
-    
-    <DownloadPdf />
+    <div className='Wrapper'>
+      <DownloadPdf />
+    </div>
     </>
   )
 }
--- src/Components/DownloadPdf.jsx
+++ src/Components/DownloadPdf.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { PDFDownloadLink } from '@react-pdf/renderer'
 import MyPage from './MyPage'
-
+import './Pdf.css'
 function DownloadPdf() {
   return (
     <>
     <PDFDownloadLink document={<MyPage />} fileName="MendZoneInvoice.pdf">
       {({ blob, url, loading, error }) =>
-        loading ? 'Loading Document...' : 'Download Now!'
+        loading ? 'Loading Document...' :<button className='btn btn-primary'>
+        Download Now!
+        </button> 
       }
     </PDFDownloadLink>
     </>
